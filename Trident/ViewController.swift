1eb489520f571e870579839aeefa8c74594d8c40
Adrian Sheppard <kelvin.tisdell@smail.astate.edu>
2019-10-10 15:08:09 -0500

    blah

---
+++ Trident/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trident
+//
+//  Created by Kelvin.Tisdell on 9/24/19.
+//  Copyright © 2019 Kelvin.Tisdell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
